02366b688d60f20837c5409e36126d5db7982927
judeadeniji <judeadeniji66@gmail.com>
2025-07-23 01:08:43 +0100

    Add UI component exports for SectionHeader, Section, Card, Accordion, CustomRadioButtons, and ContactForm

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { SectionHeader } from "./SectionHeader";
+export { Section } from "./Section";
+export { Card } from "./Card";
+export { Accordion, AccordionItem } from "./Accordion";
+export { CustomRadioButtons } from "./CustomRadioButtons";
+export { ContactForm } from "./ContactForm";
