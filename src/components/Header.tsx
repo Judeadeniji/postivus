bfc9840e33e6e0989da0cac59d62ae59884fa31e
judeadeniji <judeadeniji66@gmail.com>
2025-07-23 02:54:45 +0100

    Refactor Header component: wrap logo in Link for improved navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,31 @@
+import { Link } from "@tanstack/react-router";
 import { NavLink } from "./NavLink";
 
 export default function Header() {
 	return (
 		<header className="p-4 md:px-8 flex justify-between items-center">
-			<figure role="img">
-				<img
-					src="/logo-default.png"
-					alt="postivus logo"
-					className="h-4 md:h-6 w-auto"
-				/>
-			</figure>
+			<Link to="/">
+				<figure role="img">
+					<img
+						src="/logo-default.png"
+						alt="postivus logo"
+						className="h-4 md:h-6 w-auto"
+					/>
+				</figure>
+			</Link>
 			<nav className="hidden md:flex flex-row items-center space-x-6">
-				<NavLink to="/">About Us</NavLink>
+				<NavLink to="/about">About Us</NavLink>
 				<NavLink to="/services">Services</NavLink>
 				<NavLink to="/use-cases">Use Cases</NavLink>
 				<NavLink to="/pricing">Pricing</NavLink>
 				<NavLink to="/blog">Blog</NavLink>
 
-        <button type="button" className="border-1 border-black px-5 py-3 rounded-xl">Request a quote</button>
+				<button
+					type="button"
+					className="border-1 border-black px-5 py-3 rounded-xl"
+				>
+					Request a quote
+				</button>
 			</nav>
 		</header>
 	);
